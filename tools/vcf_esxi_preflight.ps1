ce9dbb12303a2d41c8fdca510f751632865eb8d2
Karl Newick <karlnewick@wei-lab.com>
2026-07-07 16:15:56 -0400

    chore(tools): remove vcf_esxi_preflight wrapper per request

--- tools/vcf_esxi_preflight.ps1
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env pwsh
-# Wrapper to run the top-level vcf_esxi_preflight.ps1 from the tools folder
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$parentScript = Join-Path $scriptDir '..\vcf_esxi_preflight.ps1'
-if (-not (Test-Path $parentScript)) { Write-Host "Parent script not found: $parentScript" -ForegroundColor Red; exit 1 }
-& $parentScript @Args
